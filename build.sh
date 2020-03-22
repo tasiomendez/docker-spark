333af58846ff999dc6b2671562e04ce259d227f5
Tasio Mendez <tm.tasio@gmail.com>
2020-03-22 15:00:42 +0100

    Build script for managing versions

--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
 #! /bin/sh
 
-if [ -z "${TAG}" ]; then
-  TAG=2.4.4-hadoop2.7.7 
+SPARK_VERSION=$2
+HADOOP_VERSION=$3
+
+if [ -z "${SPARK_VERSION}" ]; then
+  SPARK_VERSION=2.4.4
 fi
 
-IMAGE=tasiomendez/spark-${1%/}:${TAG%/}
+if [ -z "${HADOOP_VERSION}" ]; then
+  HADOOP_VERSION=2.7.7
+fi
+
+TAG=${SPARK_VERSION}-hadoop${HADOOP_VERSION}
+IMAGE=tasiomendez/spark-${1%/}:${TAG}
 
 cd $1
 echo '>>' building $IMAGE in $(pwd)
-docker build -t $IMAGE .
+docker build -t $IMAGE \
+    --build-arg SPARK_VERSION=${SPARK_VERSION} \
+    --build-arg HADOOP_VERSION=${HADOOP_VERSION} .
 cd -
